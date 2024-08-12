18b9194a23186a7cee0e5ed1dfed33c8e7c5464c
yosseymen <yossim@mocart.io>
2024-08-12 15:11:01 +0300

    12.08

--- app/components/editor/EditorComponent.tsx
+++ app/components/editor/EditorComponent.tsx
@@ -5,8 +5,6 @@ import SideBar from "@/components/editor/sideBar/SideBar";
 import styled from 'styled-components';
 import { EditorState, useEditor } from '@/context/useEditorContext';
 import UnityViewer from './preView/UnityViewer';
-import { useProject } from '@/context/useProjectContext';
-import axios from '@/utils/axios';
 import { useEffect } from 'react';
 
 export const EditorLayout = styled.div`
@@ -25,23 +23,6 @@ export const ResizableHandle = styled.div`
 `;
 
 const EditorComponent: React.FC = () => {
-  const { previewMode } = useProject();
-  const { sceneModel, setSceneModel } = useEditor();
-
-
-  const onClick = async () => {
-    const json = sceneModel?.exportToJson();
-    // console.log('json json json json json json',json)
-    // try {
-    //   const response = await axios.post('http://localhost:3500/preview/tytytytyty', json, {
-    //   });
-    //   console.log('response: ' + response);
-    // } catch (error) {
-    //   console.error('Error fetching project:', error);
-    //   throw error;
-    // }
-    console.log("json: " + json);
-  }
   const { editorState } = useEditor();
 
   useEffect(() => {
