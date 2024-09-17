cdec3f952086a24aa8280157724ccf1fbb8611b6
simchak <simchak@mocart.io>
2024-09-17 15:35:56 +0300

    f

---
+++ app/components/editor/viewport/models/boards/Video.bord.ts
@@ -0,0 +1,12 @@
+import { ISceneObjectOptions, ISceneObject } from '@/components/editor/types/index';
+import { BoardType } from './types';
+import { Board } from './Board';
+
+export class VideoBoard extends Board {
+    constructor(type: BoardType, options?: ISceneObjectOptions, onBoardLoaded?: () => void) {
+        super(type, options);
+    }
+
+    public addChild(sceneObject: ISceneObject): void {
+    }
+}
