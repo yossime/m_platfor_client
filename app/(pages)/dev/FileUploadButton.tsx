17f46f008cd8933004351f77b01f23c8d1a214ca
simchak <simchak@mocart.io>
2024-09-17 14:14:30 +0300

    edit files op

--- app/(pages)/dev/FileUploadButton.tsx
+++ app/(pages)/dev/FileUploadButton.tsx
@@ -1,6 +1,7 @@
 import React, { useRef } from 'react';
 import styled from 'styled-components';
 import axios from '@/utils/axios';
+import { FilePlus } from 'lucide-react';
 
 const UploadButton = styled.button`
   margin-left: 10px;
@@ -15,6 +16,22 @@ const UploadButton = styled.button`
   }
 `;
 
+const StyledButton = styled.button`
+  padding: 5px 10px;
+  margin-left: 5px;
+  background-color: #f0f0f0;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &:hover {
+    background-color: #e0e0e0;
+  }
+`;
+
 interface FileUploadButtonProps {
   folderPath: string;
   onUploadSuccess: () => void;
@@ -23,6 +40,10 @@ interface FileUploadButtonProps {
 const FileUploadButton: React.FC<FileUploadButtonProps> = ({ folderPath, onUploadSuccess }) => {
   const fileInputRef = useRef<HTMLInputElement>(null);
 
+  const normalizePath = (path: string) => {
+    return path.replace(/\/+/g, '/').replace(/^\//, '').replace(/\/$/, '');
+  };
+
   const handleFileUpload = async (event: React.ChangeEvent<HTMLInputElement>) => {
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
@@ -30,9 +51,8 @@ const FileUploadButton: React.FC<FileUploadButtonProps> = ({ folderPath, onUploa
       formData.append('file', file);
       formData.append('folderPath', folderPath);
       const filePath = `${folderPath}/${file.name}`;
-
       try {
-          await axios.post(`/dev/library?filePath=${filePath}`, formData, {
+        await axios.post(`dev/library?filePath=${normalizePath(filePath)}`, formData, {
           headers: {
             'Content-Type': 'multipart/form-data',
           },
@@ -46,7 +66,9 @@ const FileUploadButton: React.FC<FileUploadButtonProps> = ({ folderPath, onUploa
 
   return (
     <>
-      <UploadButton onClick={() => fileInputRef.current?.click()}>Upload</UploadButton>
+      <StyledButton onClick={() => fileInputRef.current?.click()}><FilePlus size={16} /></StyledButton>
+
+      {/* <UploadButton onClick={() => fileInputRef.current?.click()}>Upload</UploadButton> */}
       <input
         type="file"
         ref={fileInputRef}
